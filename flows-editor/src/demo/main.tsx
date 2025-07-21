816894898fe88814f0611c4f1666435d34438338
Guilherme da Silveira <gbasilveira@gmail.com>
2025-07-21 11:33:10 +0100

    moved the editor to a new repo

--- flows-editor/src/demo/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import SimpleDemo from './SimpleDemo'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <SimpleDemo />
-  </React.StrictMode>,
-) 
